72551eb4bbc3f04c27b126104038d326178a1010
yogeshrijal <yogeshrijal41@gmail.com>
2026-02-06 11:35:10 +0545

    bug fixed

--- Chats/cosumers.py
+++ Chats/cosumers.py
@@ -32,7 +32,7 @@ class ChatConsumer(AsyncJsonWebsocketConsumer):
 
 
 
-       self.room_group_name=f'chat_{conversation_id}'
+       self.room_group_name=f'chat_{self.conversation.id}'
 
        await self.channel_layer.group_add(
            self.room_group_name,
@@ -43,19 +43,83 @@ class ChatConsumer(AsyncJsonWebsocketConsumer):
         "type": "connection_established",
         "conversation_id": self.conversation.id
         }))
-       async  def disconnect(self,close_code):
+    async  def disconnect(self,close_code):
            if hasattr(self,'room_group_name'):
                await self.channel_layer.group_discard(
                    self.room_group_name,
                    self.channel_name
                )
-       async def recive(self,text_data):
-           data=json.loads(text_data)
-           message_content=data['meesage']
+    async def receive(self,text_data):
+            data=json.loads(text_data)
+            message_content=data['message']
 
-           saved_msg = await self.save_message(message_content)
+            saved_msg = await self.save_message(message_content)
+
+            await self.channel_layer.group_send(
+               self.room_group_name,{
+                   
+                   'type':'chat_message',
+                   'message':message_content,
+                   'sender_name':self.user.username,
+                    'sender_id':self.user.id
+
+
+
+
+               }
+
+               
+           )
            
-                 
+    async  def chat_message(self,event):
+           await self.send(text_data=json.dumps(event))
+
+
+
+
+    @database_sync_to_async  
+    def get_or_create_conversation(self,product_id):
+            try:
+                product=Product.objects.get(id=product_id)
+                chat=Conversation.objects.filter(customer=self.user,product=product).first()
+                if chat:
+                    return chat
+                
+                return Conversation.objects.create(
+                    customer=self.user,
+                    seller=product.created_by,
+                    product=product
+                )
+            except Product.DoesNotExist:
+                return None
+    @database_sync_to_async
+    def get_conversation(self,conversation_id):
+           try:
+               chat=Conversation.objects.filter(id=conversation_id).first()
+               if not chat:
+                    return None
+               if self.user==chat.customer or self.user==chat.seller:
+                   return chat
+               return None
+           except Conversation.DoesNotExist:
+               return None
+
+    @database_sync_to_async
+    def save_message(self,content):
+           return Message.objects.create(
+               conversation=self.conversation,
+               sender=self.user,
+               content=content
+           )
+          
+           
+
+
+
+
+
+
+
 
 
 
